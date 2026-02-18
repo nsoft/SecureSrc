de4b6889641c852db62a5f3c9de26fb2e7fcc947
gus <gus@needhamsoftware.com>
2026-02-18 00:16:11 -0500

    prevent annoying lucene log output. Not using vectors, don't care.

--- src/main/java/com/needhamsoftware/securesrc/Main.java
+++ src/main/java/com/needhamsoftware/securesrc/Main.java
@@ -1,10 +1,23 @@
 package com.needhamsoftware.securesrc;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
 import javax.swing.SwingUtilities;
 import com.needhamsoftware.securesrc.ui.TopFrame;
 
 public class Main {
   public static void main(String[] args) {
+    String logConfig = ".level=" + Level.OFF + '\n';
+    logConfig += "handlers=java.util.logging.ConsoleHandler\n";
+    logConfig += "java.util.logging.ConsoleHandler" + ".level=" + Level.OFF + '\n';
+    logConfig += "org.apache.lucene" + ".level=" + Level.OFF + "\n";
+    try {
+      java.util.logging.LogManager.getLogManager().readConfiguration(new java.io.ByteArrayInputStream(logConfig.getBytes(StandardCharsets.UTF_8)));
+    } catch (IOException ioe) {
+      System.err.println("cannot fully configure logging");
+      ioe.printStackTrace();
+    }
     SwingUtilities.invokeLater(() -> {
       TopFrame topFrame = new TopFrame("SecureSrc");
       topFrame.pack();
